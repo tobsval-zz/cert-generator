c57577fff7a7bf54ba64f70047f271348cc14b7e
Tobia Valerio <tobiavalerio.studio@gmail.com>
2018-07-25 15:31:09 +0100

    Comments added, minor refactoring

--- cert_generator_v3.py
+++ cert_generator_v3.py
@@ -8,9 +8,9 @@ import glob
 #Starter docx document search
 starter_doc = glob.glob('./*.docx')[0].strip('.\\')
 #Excel sheet setup
-wb = load_workbook('grelha.xlsx', data_only = True)
-sheet = wb['workshops']
-cert_amount_range = (12, 34) #Change the amount here (see excel sheet, equals to the rows interval)
+wb = load_workbook('excel_file.xlsx', data_only = True)
+sheet = wb['']
+cert_amount_range = (0, 0) #Change the amount here (see excel sheet, values in the tuple equal to the rows interval)
 
 for i in range(*cert_amount_range):
     #individual_data >> certification id, name, grade, evaluation
@@ -18,6 +18,7 @@ for i in range(*cert_amount_range):
                        sheet.cell(row=i, column=2).value,
                        sheet.cell(row=i, column=20).value,
                        sheet.cell(row=i, column=21).value)
+    #generates a new filename for each certificate (certificate owner's name + .docx extension)
     new_filename = str(individual_data[1].strip(' ')) + '.docx'
 
     #Doc and text settings setup
@@ -27,11 +28,13 @@ for i in range(*cert_amount_range):
     font.name = 'Museo 300'
     font.size = Pt(10)
 
+    #Substitute the name in the certificate's template with the owner's name
     for paragraph in doc.paragraphs:
         if '...' in paragraph.text:
             temp = paragraph.text
             paragraph.text = temp.replace('...', individual_data[1])
-            
+        
+        #Substitute the score
         if '10 (dez valores)' in paragraph.text:
             #Temp vars and variables for the different versions of the grades
             temp = paragraph.text
@@ -50,15 +53,17 @@ for i in range(*cert_amount_range):
                 else:
                     formatted_grade = str(grade) + ' (' + translated_grade[0] + ' valores) '
                     paragraph.text = temp.replace('10 (dez valores), ', formatted_grade)
-
+        
+        #Substitute the evaluation
         if 'Excelente' in paragraph.text:
             temp = paragraph.text
             paragraph.text = temp.replace('Excelente', individual_data[3])
-                
+               
+        #Substitute the certificate's ID
         if '780' in paragraph.text:
             temp = paragraph.text
             paragraph.text = temp.replace('780', str(individual_data[0]))
 
-    doc.save(individual_data[1] + '.docx')
+    doc.save(new_filename)
 
     print(individual_data, ' -> Certificate Generated')
