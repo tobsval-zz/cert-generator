56608e045ef0e1b4e7d67d867b7c201c19c9d9c4
Tobia Valerio <tobiavalerio.studio@gmail.com>
2018-07-25 15:26:49 +0100

    Added comments

--- translator.py
+++ translator.py
@@ -4,5 +4,5 @@ import n2w
 translator = Translator()
 
 def translate(num):
-    number = n2w.convert(num)
-    return translator.translate(number, dest='pt').text
+    number = n2w.convert(num) #Converts integer to its equivalent word (e.g.: 1 -> 'one')
+    return translator.translate(number, dest='pt').text #Translates the previously created word into portuguese
